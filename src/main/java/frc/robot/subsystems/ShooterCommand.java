687bf60f4b013c551103208541253f6fab8a14b3
0xRSAvoid <noambenhaim02@gmail.com>
2025-12-11 22:55:20 +0200

    nigger

--- src/main/java/frc/robot/subsystems/ShooterCommand.java
+++ src/main/java/frc/robot/subsystems/ShooterCommand.java
@@ -2,28 +2,27 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import frc.robot.RobotContainer;
 
 import java.util.function.Supplier;
 
 public class ShooterCommand {
     public static Command getSetTargetState(ShooterConstants.ShooterState targetState) {
-        return new FunctionalCommand(
-                () -> RobotContainer.SHOOTER.setTargetState(targetState),
-                () -> RobotContainer.SHOOTER.setTargetState(targetState),
-                interrupted -> RobotContainer.SHOOTER.stop(),
-                () -> false,
+        return new StartEndCommand(
+                () ->RobotContainer.SHOOTER.setTargetState(targetState),
+                () ->RobotContainer.SHOOTER.stop(),
                 RobotContainer.SHOOTER
         );
     }
 
-    public static Command getSetTargetVoltage(Supplier<Double> targetVoltage) {
-        return new FunctionalCommand(
-                () -> {},
-                () -> RobotContainer.SHOOTER.setTargetVoltage(targetVoltage.get()),
-                (interrupted) -> RobotContainer.SHOOTER.stop(),
-                () -> false,
+    public static Command getSetTargetVoltage(double targetVoltage) {
+        return new StartEndCommand(
+                () ->RobotContainer.SHOOTER.setTargetVoltage(targetVoltage),
+                () ->RobotContainer.SHOOTER.stop(),
                 RobotContainer.SHOOTER
         );
+
+
     }
 }
--- src/main/java/frc/robot/subsystems/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/ShooterConstants.java
@@ -6,7 +6,9 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.units.measure.Voltage;
 
 public class ShooterConstants {
-    private static final int MASTER_MOTOR_ID = 9, FOLLOWER_MOTOR_ID = 10;
+    private static final int
+            MASTER_MOTOR_ID = 9,
+            FOLLOWER_MOTOR_ID = 10;
 
     static final WPI_TalonSRX
             MASTER_MOTOR = new WPI_TalonSRX(MASTER_MOTOR_ID),
@@ -15,37 +17,36 @@ public class ShooterConstants {
     private static final boolean
             MASTER_MOTOR_INVERTED_VALUE = true,
             FOLLOWER_MOTOR_INVERTED_VALUE = false;
-
     private static final NeutralMode NEUTRAL_MODE = NeutralMode.Brake;
 
     private static final double VOLTAGE_LIMIT = 12;
 
-    static final int EJECT_VOLTAGE = 12;
-
-    static final int COLLECT_VOLTAGE = -12;
-
     static {
-        configMotors();
+        configureMaster();
+        configureFollower();
     }
 
-    private static void configMotors() {
+    private static void configureMaster() {
         MASTER_MOTOR.configFactoryDefault();
-        FOLLOWER_MOTOR.configFactoryDefault();
         MASTER_MOTOR.enableVoltageCompensation(true);
-        FOLLOWER_MOTOR.enableVoltageCompensation(true);
         MASTER_MOTOR.configVoltageCompSaturation(VOLTAGE_LIMIT);
-        FOLLOWER_MOTOR.configVoltageCompSaturation(VOLTAGE_LIMIT);
         MASTER_MOTOR.setNeutralMode(NEUTRAL_MODE);
-        FOLLOWER_MOTOR.setNeutralMode(NEUTRAL_MODE);
         MASTER_MOTOR.setInverted(MASTER_MOTOR_INVERTED_VALUE);
+    }
+
+    private static void configureFollower() {
+        FOLLOWER_MOTOR.configFactoryDefault();
+        FOLLOWER_MOTOR.enableVoltageCompensation(true);
+        FOLLOWER_MOTOR.configVoltageCompSaturation(VOLTAGE_LIMIT);
+        FOLLOWER_MOTOR.setNeutralMode(NEUTRAL_MODE);
         FOLLOWER_MOTOR.setInverted(FOLLOWER_MOTOR_INVERTED_VALUE);
         FOLLOWER_MOTOR.follow(MASTER_MOTOR, FollowerType.PercentOutput);
     }
+
     public enum ShooterState{
         COLLECT_STATE(-12),
         EJECT_STAGE(12),
         REST(0);
-
 
         public final double targetVoltage;
 
