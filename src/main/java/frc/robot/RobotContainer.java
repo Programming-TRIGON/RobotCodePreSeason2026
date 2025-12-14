06f21c17ec4c7f54ad05b8618fd5cc9d3e445f63
Yinon Dahan <ynnon.dahan@gmail.com>
2025-12-14 15:22:34 +0200

    Copied my tank (#4)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,17 +7,28 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.subsystems.tank.Tank;
+import frc.robot.subsystems.tank.TankCommands;
 import frc.robot.subsystems.transporter.Transporter;
 
 public class RobotContainer {
     public static final Intake INTAKE = new Intake();
     public static Shooter SHOOTER =  new Shooter();
     public static Transporter TRANSPORTER = new Transporter();
+    public static final Tank TANK = new Tank();
+    private static final CommandXboxController DRIVER_CONTROLLER = new CommandXboxController(0);
 
     public RobotContainer() {
         configureBindings();
+        TANK.setDefaultCommand(
+                TankCommands.getArcadeDriveCommand(
+                        () -> -DRIVER_CONTROLLER.getLeftY(),
+                        DRIVER_CONTROLLER::getRightX
+                )
+        );
     }
     
     private void configureBindings() {
