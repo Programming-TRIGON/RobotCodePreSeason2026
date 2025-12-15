1e88c8d8a1ec5a44a91ff64ab2935402e01f90e7
0xRSAvoid <noambenhaim02@gmail.com>
2025-12-15 15:55:05 +0200

    d

--- src/main/java/frc/robot/subsystems/transporter/TransporterConstants.java
+++ src/main/java/frc/robot/subsystems/transporter/TransporterConstants.java
@@ -43,8 +43,8 @@ public class TransporterConstants {
     }
 
     public enum TransporterState{
-        COLLECT_STATE(-12),
-        EJECT_STAGE(12),
+        COLLECT_STATE(5),
+        EJECT_STAGE(-4),
         REST(0);
 
         public final double targetVoltage;
